646f01bbf6a4ced49feba7915ab085995bf56202
Adam Goldsmith <adam.goldsmith0@gmail.com>
2023-04-12 10:06:53 +0100

    cleanup

--- src/main/java/com/anjg/stringSorter/sorterService/StringSorterService.java
+++ src/main/java/com/anjg/stringSorter/sorterService/StringSorterService.java
@@ -6,24 +6,9 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class StringSorterService {
-
-    // This is a simple function library that does not hold state, so it makes sense for it to be static
-
     private static Comparator<String> compareAlphabetically = Comparator.comparing(s -> s, String.CASE_INSENSITIVE_ORDER);
 
-    static List<String> sortAlphabetically(String[] input){
-        return Arrays.stream(input)
-                .sorted(compareAlphabetically)
-                .collect(Collectors.toList());
-    }
-
     private static Comparator<String> compareLength = Comparator.comparing(String::length);
-
-    static List<String> sortLength(String[] input){
-        return Arrays.stream(input)
-                .sorted(compareLength)
-                .collect(Collectors.toList());
-    }
 
     private static Comparator<String> fullCompare = compareLength.thenComparing(compareAlphabetically);
 
--- src/test/java/com/anjg/stringSorter/sorterService/StringSorterTests.java
+++ src/test/java/com/anjg/stringSorter/sorterService/StringSorterTests.java
@@ -7,27 +7,6 @@ import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 public class StringSorterTests {
-
-     // Left these tests here for transparency on how I worked on the problem,
-     // usually these and the methods that they point to would be
-    @Test
-    public void ListOfStrings_SortAlphabetically() {
-        String[] strings = {"ZEBRA", "banana", "Dog", "Fish"};
-
-        Iterable<String> results = StringSorterService.sortAlphabetically(strings);
-        Iterable<String> expected = new ArrayList<>(asList("banana", "Dog", "Fish", "ZEBRA"));
-        assertIterableEquals(expected, results);
-    }
-
-    @Test
-    public void ListOfStrings_SortByStringLength() {
-        String[] strings = {"ZEBRA", "banana", "Dog", "Fish"};
-
-        Iterable<String> results = StringSorterService.sortLength(strings);
-        Iterable<String> expected = new ArrayList<>(asList("Dog", "Fish", "ZEBRA", "banana"));
-        assertIterableEquals(expected, results);
-    }
-
     @Test void ListOfString_SortByStringLengthAndAlphabetically() {
         String[] strings = {"ZEBRA", "banana", "Dog", "Fish", "cat", "Drive", "NUMBER", "from"};
 
